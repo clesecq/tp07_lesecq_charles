68f91e32e9cc36c2493010161b25566681f22904
Charles LESECQ <charles@lesecq.eu>
2025-12-15 16:45:00 +0100

    feat: update main app component to use NGXS state

--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
 import { AuthService } from './auth/auth.service';
 import { FavoritesService } from './pollution/favorites.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, AsyncPipe],
   templateUrl: './app.html',
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush
